8913b5ce3141939b5e38304189bf818e9e0ca6e0
SP6 <qwer14788lll@163.com>
2020-06-01 14:58:58 +0800

    MainActivity.java：装配底部导航栏和碎片容器

--- app/src/main/java/com/example/bottomnavigationdemo/MainActivity.java
+++ app/src/main/java/com/example/bottomnavigationdemo/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.bottomnavigationdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 /**
  * @author Surface Pro 6
@@ -13,5 +19,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //初始化底部导航栏和碎片容器
+        BottomNavigationView bottomNavigationView=findViewById(R.id.bottomNavigationView);
+        NavController navController= Navigation.findNavController(this,R.id.fragment);
+        //装配，形成关联
+        AppBarConfiguration configuration=new AppBarConfiguration.Builder(bottomNavigationView.getMenu()).build();
+        NavigationUI.setupActionBarWithNavController(this,navController,configuration);
+        NavigationUI.setupWithNavController(bottomNavigationView,navController);
     }
 }
