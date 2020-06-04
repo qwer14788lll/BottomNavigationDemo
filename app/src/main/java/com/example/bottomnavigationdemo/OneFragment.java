088ed601b508d4947d6dea2de4b5b6c1f0ff123b
SP6 <qwer14788lll@163.com>
2020-06-04 08:53:51 +0800

    OneFragment实现功能：点击图标，发生旋转

--- app/src/main/java/com/example/bottomnavigationdemo/OneFragment.java
+++ app/src/main/java/com/example/bottomnavigationdemo/OneFragment.java
@@ -2,6 +2,7 @@ package com.example.bottomnavigationdemo;
 
 import androidx.lifecycle.ViewModelProviders;
 
+import android.animation.ObjectAnimator;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -11,6 +12,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 /**
  * @author Surface Pro 6
@@ -18,6 +20,7 @@ import android.view.ViewGroup;
 public class OneFragment extends Fragment {
 
     private OneViewModel mViewModel;
+    private ImageView mImageView;
 
     public static OneFragment newInstance() {
         return new OneFragment();
@@ -26,7 +29,9 @@ public class OneFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.one_fragment, container, false);
+        View view = inflater.inflate(R.layout.one_fragment, container, false);
+        mImageView = view.findViewById(R.id.imageView);
+        return view;
     }
 
     @Override
@@ -34,6 +39,20 @@ public class OneFragment extends Fragment {
         super.onActivityCreated(savedInstanceState);
         mViewModel = ViewModelProviders.of(this).get(OneViewModel.class);
         // TODO: Use the ViewModel
+        //ofFloat:对象，动画类型，数值
+        final ObjectAnimator objectAnimator =ObjectAnimator.ofFloat(mImageView,"rotation",0,0);
+        objectAnimator.setDuration(500);
+        mImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!objectAnimator.isRunning())
+                {
+                    float f=mImageView.getRotation();
+                    objectAnimator.setFloatValues(f,f+100);
+                    objectAnimator.start();
+                }
+            }
+        });
     }
 
 }
